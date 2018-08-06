4d770f069bc448aa0dc45e17266d6a63239ba28e
NanamiTakayama <ns.potable@gmail.com>
2018-08-06 18:32:59 +0900

    first commit

---
+++ IoT.py
@@ -0,0 +1,18 @@
+# -*- Coding: utf-8 -*-
+
+import os
+import subprocess
+import datetime
+from time import sleep
+
+def main():
+    dt_now = datetime.datetime.now()
+    print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "IoT Device Online")
+    print(os.environ.get('LANG'))
+    #print(os.environ.get('RemoteENV'))
+
+
+if __name__ == '__main__':
+    while True:
+        main()
+        sleep(5)
---
+++ README.md
@@ -0,0 +1,1 @@
+# IoT_skill_pack_starting
