1bec4891de6fcf8cc75a83ef78a5686d256721f0
RyoIkuta <58468874+RyoIkuta@users.noreply.github.com>
2019-12-03 20:11:53 +0900

    Update IoT.py

--- IoT.py
+++ IoT.py
@@ -1,18 +1,16 @@
 # -*- Coding: utf-8 -*-
-
 import os
 import subprocess
 import datetime
 from time import sleep
 
 def main():
-    dt_now = datetime.datetime.now()
-    print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "IoT Device Online")
-    print(os.environ.get('LANG'))
-    #print(os.environ.get('RemoteENV'))
-
+  dt_now = datetime.datetime.now()
+  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "Hello World from IoT device")
+  print(os.environ.get('LANG'))
+  #print(os.environ.get('RemoteENV'))
 
 if __name__ == '__main__':
-    while True:
-        main()
-        sleep(5)
+  while True:
+    main()
+    sleep(5)
